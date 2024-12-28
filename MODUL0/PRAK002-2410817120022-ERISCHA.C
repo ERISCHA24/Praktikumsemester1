1fb1f38cde7ed10952930f25e2ae5a158db9eb43
ERISCHA24 <2410817120022@mhs.ulm.ac.id>
2024-12-28 15:53:58 +0700

    Add files via upload

---
+++ MODUL0/PRAK002-2410817120022-ERISCHA.C
@@ -0,0 +1,7 @@
+#include <stdio.h> 
+int main () { 
+    printf("Selamat Pagi, ERISCHA \n"); 
+    printf("Selamat Siang, ERISCHA \n"); 
+    printf("Selamat Malam, ERISCHA \n"); 
+    return 0; 
+} 
---
+++ MODUL0/PRAK004-2410817120022-ERISCHA.C
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+int main () { 
+    printf("###################\n"); 
+    printf("#                 #\n"); 
+    printf("# ERISCHA MARSELA #\n"); 
+    printf("#  2410817120022  #\n"); 
+    printf("#                 #\n"); 
+    printf("###################\n"); 
+    return 0; 
+} 
