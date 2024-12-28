ed474f6e2f678e2074e2735f1c40b1ee3507006d
ERISCHA24 <2410817120022@mhs.ulm.ac.id>
2024-12-28 15:57:03 +0700

    Add files via upload

---
+++ MODUL5/PRAK502-2410817120022-ERISCHA.C
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include <math.h> 
+int hitung(int nilai1, int nilai2) { 
+    return nilai1 + nilai2; 
+} 
+int mutlak(int angka) { 
+    return abs(angka); 
+} 
+int main() { 
+    int a, b, c, d; 
+    scanf("%d", &a); 
+    scanf("%d", &b); 
+    scanf("%d", &c); 
+    scanf("%d", &d); 
+    int Hasil = hitung(c, d) - hitung(a, b); 
+    printf("%d", mutlak(Hasil)); 
+    return 0; 
+} 
